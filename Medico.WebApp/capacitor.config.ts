6810f8f0e698d7406a9bb381b3e9f8340fa9d5c4
Omkar Baswat <omkarbaswat@outlook.com>
2026-02-14 20:46:53 +0530

    android config breaking gradle currently

---
+++ Medico.WebApp/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.omkar.clinics',
+  appName: 'clinics-managment-frontend',
+  webDir: 'dist/clinics-managment-frontend',
+  bundledWebRuntime: false
+};
+
+export default config;
